d55e9569b303f6b092992f1f5c21a103c8eca48b
Kollol <taher10321@gmail.com>
2018-08-26 02:25:20 +0600

    making better

--- server.js
+++ server.js
@@ -4,27 +4,28 @@ const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(port).sockets;
 
 // DB Config 
-const db = require('./config/database');
+// const db1 = require('./config/database');
+
 // Connect to mongo
-mongo.connect(db.mongoURI, function(err, db){
-    if(err){
+mongo.connect('mongodb://Brad:turnover121@ds131932.mlab.com:31932/mongochat_for_test', function (err, db) {
+    if (err) {
         throw err;
     }
 
     console.log('MongoDB connected...');
 
     // Connect to Socket.io
-    client.on('connection', function(socket){
+    client.on('connection', function (socket) {
         let chat = db.collection('chats');
 
         // Create function to send status
-        sendStatus = function(s){
+        sendStatus = function (s) {
             socket.emit('status', s);
         }
 
         // Get chats from mongo collection
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-            if(err){
+        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
+            if (err) {
                 throw err;
             }
 
@@ -33,17 +34,17 @@ mongo.connect(db.mongoURI, function(err, db){
         });
 
         // Handle input events
-        socket.on('input', function(data){
+        socket.on('input', function (data) {
             let name = data.name;
             let message = data.message;
 
             // Check for name and message
-            if(name == '' || message == ''){
+            if (name == '' || message == '') {
                 // Send error status
                 sendStatus('Please enter a name and message');
             } else {
                 // Insert message
-                chat.insert({name: name, message: message}, function(){
+                chat.insert({ name: name, message: message }, function () {
                     client.emit('output', [data]);
 
                     // Send status object
@@ -56,9 +57,9 @@ mongo.connect(db.mongoURI, function(err, db){
         });
 
         // Handle clear
-        socket.on('clear', function(data){
+        socket.on('clear', function (data) {
             // Remove all chats from collection
-            chat.remove({}, function(){
+            chat.remove({}, function () {
                 // Emit cleared
                 socket.emit('cleared');
             });
